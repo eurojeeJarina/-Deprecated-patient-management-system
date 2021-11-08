7f02da1e714aaab169ad7ec26da6ab78665b0fff
nebo01 <eurojee.jarina@owasp.org>
2021-11-08 23:13:10 +0000

    <perf: added new variables for database connection>(DBconnect.java): add new variables
    <add new variable for database connection>

--- src/main/java/com/example/patientmanagesystem/DBConnect.java
+++ src/main/java/com/example/patientmanagesystem/DBConnect.java
@@ -2,7 +2,9 @@ package com.example.patientmanagesystem;
 
 public class DBConnect {
 
-
+    private static final String _URL = "jdbc:sqlserver://localhost\\sqlexpress";
+    private static final String _USER = "sa";
+    private static final String _PASSWORD = "blank";
 
     public DBConnect(String _URL, String _USER, String _PASSWORD) {
 
